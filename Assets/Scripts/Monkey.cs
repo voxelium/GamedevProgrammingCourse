93ec67a69737d0b264853a9c414f74823f67811f
VOXELIUM <52653488+voxelium@users.noreply.github.com>
2024-06-22 21:51:49 +0400

    first homework

---
+++ Assets/Scripts/Monkey.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monkey : MonoBehaviour
+{
+
+    [SerializeField] private string _name = "Player";
+    [SerializeField] private float _moveSpeed = 1.3f;
+    [SerializeField] private int _collectedBananas = 0;
+    [SerializeField] private bool _isBananasCollected = false;
+    [SerializeField] private float _size = 1;
+    [SerializeField] private float _jumpSpeed = 5f;
+
+    private Rigidbody _rigidbody;
+
+    void Start()
+    {
+        Debug.Log("Monkey's name: " + _name);
+        Debug.Log("Monkey's speed: " + _moveSpeed);
+        Debug.Log("Monkey's collected bananas: " + _collectedBananas);
+        Debug.Log("All the bananas have been collected: " + _isBananasCollected);
+        Debug.Log("Monkey's size: " + _size);
+        Debug.Log("Monkey's jump speed: " + _jumpSpeed);
+
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Floor"))
+        {
+            Vector3 jumpVelocity = new Vector3(0f, _jumpSpeed, 0);
+            _rigidbody.velocity = jumpVelocity;
+        }
+    }
+
+
+}
